12d558163ce8413dfd825e4c1089e836da9e63fd
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-05-17 16:06:09 +0100

    - Final Commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Display an H1 message on screen using Vue JS structure "data"
+// Bonus:Add an image in the same way.
+const app = new Vue({
+    el: "#root",
+    data:{
+        message: "Join Vue JS",
+        logo: "./assets/img/Sith.Lord.png"
+    }
+})
